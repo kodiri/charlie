583c0d089405616c981dc818a502ba43f7734b20
unknown <nicolascc1@hotmail.com>
2019-11-28 15:51:20 +0000

    fetch code was placed into its own component to clear App.js

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from 'react';
-//import Data from '../frontend/Data.js'
+import React from 'react';
+import getData from './getData'
 import '../frontend/App.css';
-import Axios from 'axios';
+import UserPage from './UserPage/UserPage';
 
 function App() {
 
-  const [ info, setInfo ] = useState([]);
-  const [ isLoaded, setisLoaded ] = useState(false);
-
-  useEffect(() => {
-    Axios.get('http://api.football-data.org/v2/competitions/2001/teams', {
-        headers: {'X-Auth-Token': '0154c5dc13514833a457779bbaafdd13', 'Content-Type': 'application/json'},
-      })
-      .then(data=> setInfo(data))
-  }, []);
-
   return (
     <div className="App">
-      <h1>Info</h1>
-      {
-        info ? <h2>Data has loaded</h2> : <h3>It is not working</h3>
-      }
-      {/* {
-        Data().map(leagues => leagues.competitions.map(entry => <p>{entry.name}</p>) )
-      }       */}
-
+      <h1>FootApp</h1>
+      <UserPage />
     </div>
   );
 }
---
+++ src/frontend/UserPage/UserPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function UserPage() {
+
+    return (
+        <>
+            <h1>Welcome User</h1>
+            
+        </>
+    )
+}
+
