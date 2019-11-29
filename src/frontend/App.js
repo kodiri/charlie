008df7b3e6cbc7b62bcb5d550fe70edd2dd67377
unknown <nicolascc1@hotmail.com>
2019-11-29 15:33:12 +0000

    Deployment on heroku

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import getData from './getData'
+// import Data from './Data'
 import '../frontend/App.css';
 import UserPage from './UserPage/UserPage';
+import PremTable from './PremTable/PremTable';
+import Homepage from './Homepage/Homepage';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>FootApp</h1>
-      <UserPage />
+      <div>
+              <Homepage />
+      </div>
+      {/* <UserPage /> */}
+      <div className='table'>
+              <PremTable />
+      </div>
+      {/* <Data /> */}
     </div>
   );
 }
