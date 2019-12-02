f795068661eaca210e478f1cbb0d3da86d2e2db3
unknown <nicolascc1@hotmail.com>
2019-12-02 09:20:50 +0000

    Home page and prem table integrated

--- src/frontend/Homepage/Homepage.js
+++ src/frontend/Homepage/Homepage.js
@@ -6,26 +6,28 @@ import './Homepage.css';
 
 export default function Homepage() {
     return (
-      <>
-            <div className='logo'>
-                <img src={logo} alt='logo'></img>
+        <>
+            <div className='App'>
+                <div className='logo'>
+                    <img src={logo} alt='logo'></img>
+                </div>
+                <div className='players'>
+                    <img className='cris' src={cristiano} alt='Cris' />
+                    <img className='messi' src={messi} alt='Messi' />
+                </div>
+                <div className='form'>
+                    <form>
+                        <input className='name' type='text' placeholder='Please enter your name' />
+                        <br></br>
+                        <select className='select'>
+                            <option>REAL MADRID</option>
+                            <option>BARCELONA</option>
+                            <option>MAN CITY</option>
+                        </select>
+                        <button className='button'>ENTER</button>
+                    </form>
+                </div>
             </div>
-            <div className='players'>
-                <img className='cris' src={cristiano} alt='Cris' />
-                <img className='messi' src={messi} alt='Messi' />
-            </div>
-            <div className='form'>
-                <form>
-                    <input className='name' type='text' placeholder='Please enter your name' />
-                    <br></br>
-                    <select className='select'>
-                        <option>REAL MADRID</option>
-                        <option>BARCELONA</option>
-                        <option>MAN CITY</option>
-                    </select>
-                    <button className='button'>ENTER</button>
-                </form>
-            </div>
-      </>
+        </>
     )
 }
--- src/frontend/UserPage/UserPage.js
+++ src/frontend/UserPage/UserPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PremTable from '../PremTable/PremTable'
 
 export default function UserPage() {
 
     return (
         <>
             <h1>Welcome User</h1>
-            
+            <PremTable />
         </>
     )
 }
