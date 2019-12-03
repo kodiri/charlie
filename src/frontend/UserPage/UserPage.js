13e19d8b85171a47ae993b2cae88862a3a421f63
unknown <nicolascc1@hotmail.com>
2019-12-03 16:07:14 +0000

    Integrated Menu into Userpage

--- src/frontend/UserPage/UserPage.js
+++ src/frontend/UserPage/UserPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PremTable from '../PremTable/PremTable'
+import MenuBar from '../components/BurgerMenu/BurgerMenu';
 
 export default function UserPage() {
 
     return (
         <>
+            <MenuBar />
             <h1>Welcome User</h1>
             <PremTable />
         </>
