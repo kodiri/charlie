f81e9f5ed91ba5d27cdbffc21867760c93176eff
unknown <nicolascc1@hotmail.com>
2019-12-03 14:21:56 +0000

    homemade images need fixing

--- src/frontend/Homepage/Homepage.js
+++ src/frontend/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../Images/logo.png';
 import messi from '../Images/messi.png';
 import cristiano from '../Images/cris.png';
@@ -8,21 +9,24 @@ export default function Homepage() {
     return (
         <>
             <div className='App'>
-                <div className='logo'>
-                    <img src={logo} alt='logo'></img>
-                </div>
+
                 <div className='layout'>
                     <img className='cris' src={cristiano} alt='Cris' />
                     <div className='form'>
                         <form>
+                            <div className='logo center'>
+                                <img src={logo} alt='logo'></img>
+                            </div>
                             <input className='name' type='text' placeholder='Please enter your name' />
                             <br></br>
-                            <select className='select'>
+                            <select className='name'>
                                 <option>REAL MADRID</option>
                                 <option>BARCELONA</option>
                                 <option>MAN CITY</option>
                             </select>
-                            <button className='button'>ENTER</button>
+                            <div className='center'>
+                                <Link to='/userpage' className='link'>ENTER</Link>
+                            </div>
                         </form>
                     </div>
                     <img className='messi' src={messi} alt='Messi' />
