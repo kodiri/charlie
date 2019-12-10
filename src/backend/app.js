8199fd7f01e414a13c7b47cc3f2e918ae3c6b180
unknown <nicolascc1@hotmail.com>
2019-12-10 14:07:48 +0000

    testing api in deployment

--- src/backend/app.js
+++ src/backend/app.js
@@ -8,10 +8,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../../build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
-
 app.get('/rest/competitions', (req, res) => {
   Axios.get('http://api.football-data.org/v2/competitions/2021/matches', {
     headers: { 'X-Auth-Token': '0154c5dc13514833a457779bbaafdd13', 'Content-Type': 'application/json' },
@@ -20,3 +16,7 @@ app.get('/rest/competitions', (req, res) => {
   })
 });
 app.listen(port, () => console.log(`${port}`));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
