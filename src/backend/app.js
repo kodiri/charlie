b707980c64171ee37801e97ca653827cef78b144
unknown <nicolascc1@hotmail.com>
2019-12-17 15:03:49 +0000

    Changed http to https

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,12 +9,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/rest/competitions', (req, res) => {
-  Axios.get('http://api.football-data.org/v2/competitions/2021/matches', {
+  Axios.get('https://api.football-data.org/v2/competitions/2021/matches', {
     headers: { 'X-Auth-Token': '0154c5dc13514833a457779bbaafdd13', 'Content-Type': 'application/json' },
   }).then(response => {
     res.send(response.data)
   })
 });
+
 app.listen(port, () => console.log(`${port}`));
 
 app.get('*', function (req, res) {
