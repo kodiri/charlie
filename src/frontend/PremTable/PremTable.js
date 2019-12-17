daa764adb89b5b4c14b765f14aabf6f3fe0e5094
unknown <nicolascc1@hotmail.com>
2019-12-17 14:59:05 +0000

    changed http to https

--- src/frontend/PremTable/PremTable.js
+++ src/frontend/PremTable/PremTable.js
@@ -7,7 +7,7 @@ export default function PremTable() {
   const [info, setInfo] = useState([]);
 
   useEffect(() => {
-    Axios.get('http://api.football-data.org/v2/competitions/2021/standings', {
+    Axios.get('https://api.football-data.org/v2/competitions/2021/standings', {
       headers: { 'X-Auth-Token': '0154c5dc13514833a457779bbaafdd13', 'Content-Type': 'application/json' },
     })
       .then(data => setInfo(data.data.standings[0].table));
