498970ae0a6a4fb9e1f6a11daf5057e98df658c5
unknown <nicolascc1@hotmail.com>
2019-12-19 17:59:58 +0000

    team logos added to userpage

--- src/frontend/Data.js
+++ src/frontend/Data.js
@@ -1,26 +1,47 @@
+import Arsenal from './TeamLogos/arsenal.png';
+import AstonVila from './TeamLogos/astonvilla.png';
+import Bournemouth from './TeamLogos/bournemouth.png';
+import Brighton from './TeamLogos/brighton.png';
+import Burnley from './TeamLogos/burnley.png';
+import Chelsea from './TeamLogos/chelsea.png';
+import CrystalPalace from './TeamLogos/crystalpalace.png';
+import Everton from './TeamLogos/everton.png';
+import Leicester from './TeamLogos/leicester.png';
+import Liverpool from './TeamLogos/liverpool.png';
+import ManCity from './TeamLogos/mancity.png';
+import ManUtd from './TeamLogos/manutd.png';
+import Newcastle from './TeamLogos/newcastle.png';
+import Norwich from './TeamLogos/norwich.png';
+import Sheffield from './TeamLogos/sheffield.png';
+import Southampton from './TeamLogos/southampton.png';
+import Tottenham from './TeamLogos/tottenham.png';
+import Watford from './TeamLogos/watford.png';
+import WestHam from './TeamLogos/westham.png';
+import Wolves from './TeamLogos/wolves.png';
+
 export default function Data() {
     return [
             {name: "-- Please select a team --", id: 1111 },  
-            {name: "Arsenal", id: 57, rating: 5 },
-            {name: "Aston Villa", id: 58, rating: 4 },
-            {name: "Chelsea", id: 61, rating: 6},
-            {name: "Everton", id: 62, rating: 4 },
-            {name: "Liverpool", id: 64, rating: 9},
-            {name: "Manchester City", id: 65, rating: 8},
-            {name: "Manchester United", id: 66, rating: 7},
-            {name: "Newcastle United", id: 67, rating: 4 },
-            {name: "Norwich City", id: 68, rating: 4 },
-            {name: "Tottenham Hotspur", id: 73, rating: 7},
-            {name: "Wolverhampton Wanderers", id: 76, rating: 5},
-            {name: "Burnley", id: 328, rating: 4},
-            {name: "Leicester City", id: 338, rating: 7},
-            {name: "Southampton", id: 340, rating: 3 },
-            {name: "Watford City", id: 346,  rating: 2},
-            {name: "Crystal Palace", id: 354, rating: 4},
-            {name: "Sheffield United", id: 356, rating: 4},
-            {name: "Brighton & Hove Albion", id: 397, rating: 4},
-            {name: "West Ham United", id: 563, rating: 3},
-            {name: "AFC Bournemouth", id: 1044, rating: 3},
+            {name: "Arsenal", id: 57, rating: 5, logo: Arsenal },
+            {name: "Aston Villa", id: 58, rating: 4, logo: AstonVila },
+            {name: "Chelsea", id: 61, rating: 6, logo: Chelsea },
+            {name: "Everton", id: 62, rating: 4, logo: Everton },
+            {name: "Liverpool", id: 64, rating: 9, logo: Liverpool },
+            {name: "Manchester City", id: 65, rating: 8, logo: ManCity },
+            {name: "Manchester United", id: 66, rating: 7, logo: ManUtd },
+            {name: "Newcastle United", id: 67, rating: 4, logo: Newcastle },
+            {name: "Norwich City", id: 68, rating: 4, logo: Norwich },
+            {name: "Tottenham Hotspur", id: 73, rating: 7, logo: Tottenham },
+            {name: "Wolverhampton Wanderers", id: 76, rating: 5, logo: Wolves },
+            {name: "Burnley", id: 328, rating: 4, logo: Burnley },
+            {name: "Leicester City", id: 338, rating: 7, logo: Leicester },
+            {name: "Southampton", id: 340, rating: 3, logo: Southampton },
+            {name: "Watford City", id: 346,  rating: 2, logo: Watford },
+            {name: "Crystal Palace", id: 354, rating: 4, logo: CrystalPalace },
+            {name: "Sheffield United", id: 356, rating: 4, logo: Sheffield },
+            {name: "Brighton & Hove Albion", id: 397, rating: 4, logo: Brighton },
+            {name: "West Ham United", id: 563, rating: 3, logo: WestHam },
+            {name: "AFC Bournemouth", id: 1044, rating: 3, logo: Bournemouth },
         ]
     }
 
